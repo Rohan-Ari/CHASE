f6d32b40d85b54ced4b1ae4feb01140d268fb277
bs <pikachuchan1111@gmail.com>
2025-02-24 13:57:57 -0500

    added ble stuff

--- src/main.c
+++ src/main.c
@@ -1,6 +1,18 @@
 #include <zephyr/kernel.h>
+#include <logging/log.h>
+#include <dk_buttons_and_leds.h>
+#include "remote.h"
+
+#define LOG_MODULE_NAME app
+LOG_MODULE_REGISTER(LOG_MODULE_NAME)
 
 int main(void)
 {
+        int err;
+        err = bluetooth_init();
+        if(err) {
+                LOG_ERR("Bluetooth init returned %d", err);
+        }
+        LOG_INF("Running...")
         return 0;
 }
---
+++ src/remote.h
@@ -0,0 +1,9 @@
+#include <zephyr/kernel.h>
+#include <logging/log.h>
+
+#include <bluetooth/bluetooth.h>
+#include <bluetooth/uuid.h>
+#include <bluetooth/gat.h>
+#include <bluetooth/hci.h>
+
+int bluetooth_init(void);
